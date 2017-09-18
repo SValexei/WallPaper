f9ce8e0a6e8a0a18ea134d5f79c92cf573321df4
SValexei <al.stepanov.vl@gmail.com>
2017-09-18 22:05:39 +0000

    changed absolute adress

--- wallPaper.py
+++ wallPaper.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
 import json, random, subprocess
+from pathlib import Path
 
 jsonList = []
 randomId = random.randint(0, 1000)
+home = str(Path.home())
 
-with open('/home/alexei/Scripts/WallPaper/list') as json_data:
+with open(home +'/Scripts/WallPaper/list') as json_data:
     jsonList = json.load(json_data)
 
 imageUrl = jsonList[randomId].get('post_url')
 
-subprocess.run("wget -O /home/alexei/Scripts/WallPaper/randomImage.jpg " + imageUrl +"/download?force=true", shell=True)
-subprocess.run("feh --bg-scale /home/alexei/Scripts/WallPaper/randomImage.jpg", shell=True)
+subprocess.run("wget -O " + home + "/Scripts/WallPaper/randomImage.jpg " + imageUrl +"/download?force=true", shell=True)
+subprocess.run("feh --bg-scale " + home + "/Scripts/WallPaper/randomImage.jpg", shell=True)
